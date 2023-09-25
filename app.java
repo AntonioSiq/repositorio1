08edb2ef7bb368ed1b2c75434a8f80a7cc8778dc
AntonioSiq <143460296+AntonioSiq@users.noreply.github.com>
2023-09-25 17:51:21 -0300

    teste 2

--- app.java
+++ app.java
@@ -1,8 +1,2 @@
-java.ultil.Scanner;
+
 console.log("rodando o sistema")
-
-public static void main(String[] args){
-    Scanner in = new Scanner(System.in);
-
-    
-}
---
+++ index.java
@@ -0,0 +1,8 @@
+java.ultil.Scanner;
+public class a{
+public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+
+    
+}
+}
